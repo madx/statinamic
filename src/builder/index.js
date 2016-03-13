8830731bd0dba4bbc3dfae59f054339414b48410
Khoa Nguyen <hi@khoanguyen.me>
2016-03-13 16:24:14 +0700

    Fix lint

--- src/builder/index.js
+++ src/builder/index.js
@@ -82,7 +82,7 @@ export default async function(options) {
     }
   }
   catch (err) {
-    setImmedialy(() => {
+    setImmediate(() => {
       throw err
     })
   }
--- src/builder/webpack.js
+++ src/builder/webpack.js
@@ -1,5 +1,5 @@
 import webpack from "webpack"
-import color from "chalk"
+// import color from "chalk"
 
 export default (webpackConfig, log) => {
   return new Promise((resolve, reject) => {
@@ -7,21 +7,18 @@ export default (webpackConfig, log) => {
     compiler.run((err, stats) => {
       const jsonStats = stats.toJson()
 
-      debug('Webpack compile completed.')
-      debug(stats.toString(statsFormat))
-
       if (err) {
-        debug('Webpack compiler encountered a fatal error.', err)
+        log("Webpack compiler encountered a fatal error.", err)
         return reject(err)
       }
       else if (jsonStats.errors.length > 0) {
-        debug('Webpack compiler encountered errors.')
-        debug(jsonStats.errors.join('\n'))
-        return reject(new Error('Webpack compiler encountered errors'))
+        log("Webpack compiler encountered errors.")
+        log(jsonStats.errors.join("\n"))
+        return reject(new Error("Webpack compiler encountered errors"))
       }
       else if (jsonStats.warnings.length > 0) {
-        debug('Webpack compiler encountered warnings.')
-        debug(jsonStats.warnings.join('\n'))
+        log("Webpack compiler encountered warnings.")
+        log(jsonStats.warnings.join("\n"))
       }
       resolve(jsonStats)
     })
